fd4d3fe3a6ddf3ce35679054695e214640cef04d
Shekx <shichering@gmail.com>
2015-09-18 23:16:11 +0800

    for solving dirty worktree

--- src/model/vo/VideoVO.java
+++ src/model/vo/VideoVO.java
@@ -2,18 +2,36 @@ package model.vo;
 
 import java.text.SimpleDateFormat;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class VideoVO {
+	@XmlElement(required = true)
 	private int videoId;
+	@XmlElement(required = true)
 	private int memberId;
+	@XmlElement(required = true)
 	private String videoWebsite;
+	@XmlElement(required = true)
 	private String videoClassName;
+	@XmlElement(required = true)
 	private String videoTitle;
+	@XmlElement(required = true)
 	private String videoName;
+	@XmlElement(required = true)
 	private String videoPath;
+	@XmlElement(required = true)
 	private java.util.Date videoUploadTime;
+	@XmlElement(required = true)
 	private long videoWatchTimes;
+	@XmlElement(required = true)
 	private String videoDescription;
+	@XmlElement(required = true)
 	private java.util.Date videoDescriptionModifyTime;
+	@XmlElement(required = true)
 	private MemberVO member;
 	
 	@Override
